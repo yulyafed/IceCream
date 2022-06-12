638cb4f8ec149187046b818bc48b9e10124d4bc5
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2022-06-12 19:56:12 +0300

    add fixbag

--- src/js/modal-franchise.js
+++ src/js/modal-franchise.js
@@ -11,6 +11,5 @@ refs.closeModFrBtn.addEventListener("click", toggleModFr);
 
 function toggleModFr() {
     refs.modfr.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
 }
 })();
