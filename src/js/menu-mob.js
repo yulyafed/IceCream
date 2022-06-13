00faec4acb801cc2bf13dff3675a776300588989
NazarBezpaliuk <nazardxvx@gmail.com>
2022-06-13 11:21:34 +0300

    fix-hero-header

--- src/js/menu-mob.js
+++ src/js/menu-mob.js
@@ -2,15 +2,15 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
+    closeAnchorLink: document.querySelector('[data-anchor-close]'),
+    modal: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
   };
-
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden-mob');
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.closeAnchorLink.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-mob');
     refs.body.classList.toggle('no-scroll');
   }
 })();
